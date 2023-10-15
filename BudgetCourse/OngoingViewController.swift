aed330fac088d462b6da44663f82d3b41f5e3052
BadWolf20 <Roman.Pichugin20@gmail.com>
2023-10-15 14:07:12 +0700

    Basic setup

---
+++ BudgetCourse/OngoingViewController.swift
@@ -0,0 +1,62 @@
+//
+//  OngoingViewController.swift
+//  BudgetCourse
+//
+//  Created by Roman on 14.10.2023.
+//
+
+import UIKit
+
+class OngoingViewController: UIViewController {
+
+    // MARK: - Properties
+
+
+
+    // MARK: - Components
+
+
+
+    // MARK: - Initializers
+
+
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+
+    // MARK: - Setup
+    private func setupUI() {
+        setupHierarchy()
+        setupConstraints()
+        setupComponents()
+        setupText()
+    }
+
+    private func setupHierarchy() {
+    }
+    private func setupComponents() {
+
+    }
+
+    private func setupText() {
+
+    }
+
+    private func setupConstraints() {
+    }
+
+    // MARK: - Update
+
+    // MARK: - Actions
+
+
+
+    // MARK: - Functions
+
+
+
+}
